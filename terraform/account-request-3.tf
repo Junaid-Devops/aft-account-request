ace9381b057e4159bd768047cb0ccdbf68f66184
Junaid Majumdar <jmajumdar@agero.com>
2026-06-08 10:43:26 -0400

    adding new account

---
+++ terraform/account-request-3.tf
@@ -0,0 +1,35 @@
+module "account_request_03" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "jmajumdar+testaft4@yahoo.com"
+    AccountName               = "testaft4"
+    ManagedOrganizationalUnit = "prod (ou-pddm-qw1qptnw)"
+    SSOUserEmail              = "jmajumdar+testaft2@yahoo.com"
+    SSOUserFirstName          = "Junaid"
+    SSOUserLastName           = "Majumdar"
+  }
+
+  account_tags = {
+    "ABC:Owner"       = "jmajumdar+testaft4@yahoo.com"
+    "ABC:Division"    = "ENT"
+    "ABC:Environment" = "Dev"
+    "ABC:CostCenter"  = "123456"
+    "ABC:Vended"      = "true"
+    "ABC:DivCode"     = "102"
+    "ABC:BUCode"      = "ABC003"
+    "ABC:Project"     = "123456"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "AWS Control Tower Lab"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform (AFT)"
+  }
+
+  custom_fields = {
+    custom1 = "a"
+    custom2 = "b"
+  }
+
+  account_customizations_name = "SANDBOX"
+}
